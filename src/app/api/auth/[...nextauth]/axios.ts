9c8d8394307c62091e4073528e00397f74b249d5
gt-rg-1401 <raphael.gomes@gaintheory.com>
2025-06-02 18:12:15 -0300

    fix: remove commented-out signOut logic in axios interceptor

--- src/app/api/auth/[...nextauth]/axios.ts
+++ src/app/api/auth/[...nextauth]/axios.ts
@@ -6,10 +6,6 @@ const api = axios.create({
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
-      // signOut();
-      // window.location.href = "/login";
-    }
     return Promise.reject(error);
   }
 );
